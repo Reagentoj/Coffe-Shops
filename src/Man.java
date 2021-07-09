cbe26fd8a9c84ec0677e6d0c991a4f92fdae9ff5
Decard <86321722+Astemir2147@users.noreply.github.com>
2021-07-09 22:42:28 +0300

    Delete Man.java

--- src/Man.java
+++
@@ -1,9 +0,0 @@
-public class Man {
-    private long id;             /// уникальный идентефикатор
-    private String name;         /// имя
-
-    public Man(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-}
